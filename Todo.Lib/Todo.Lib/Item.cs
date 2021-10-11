cd53c77c804993c109d537e3fe9e60f9a6b7f124
Eline Moerman <Eline.Moerman@realdolmen.com>
2021-10-11 14:54:07 +0200

    Initial lib commit

---
+++ Todo.Lib/Todo.Lib/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Todo.Lib
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public UrgencyLevel UrgencyLevel { get; set; }
+        public DateTime Deadline { get; set; }
+        public bool IsDone { get; set; }
+
+        public Item()
+        {
+
+        }
+
+        public Item(int id, string title, string content, UrgencyLevel urgencylevel, DateTime deadline, bool isDone)
+        {
+            Id = id;
+            Title = title;
+            Content = content;
+            UrgencyLevel = urgencylevel;
+            Deadline = deadline;
+            IsDone = isDone;
+        }
+    }
+}
