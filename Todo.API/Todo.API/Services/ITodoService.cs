df9aad17a1ae665c0e318376de45b966132fb858
Mika Debelder <Mika.Debelder@realdolmen.com>
2021-10-11 15:07:00 +0200

    Init

---
+++ Todo.API/Todo.API/Services/ITodoService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todo.Lib;
+
+namespace Todo.API.Services
+{
+    interface ITodoService
+    {
+        List<Item> GetAll();
+    }
+}
---
+++ Todo.API/Todo.API/Services/TodoService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todo.Lib;
+
+namespace Todo.API.Services
+{
+    public class TodoService : ITodoService
+    {
+        public List<Item> TodoItems { get; set; } = new List<Item>()
+        {
+            new Item(1, "stofzuigen", "beuzak", UrgencyLevel.URGENT, DateTime.Now, false),
+            new Item(2, "stofzuigen", "beuzak", UrgencyLevel.URGENT, DateTime.Now, false),
+            new Item(3, "stofzuigen", "beuzak", UrgencyLevel.URGENT, DateTime.Now, false),
+            new Item(4, "stofzuigen", "beuzak", UrgencyLevel.URGENT, DateTime.Now, false),
+            new Item(5, "stofzuigen", "beuzak", UrgencyLevel.URGENT, DateTime.Now, false),
+        };
+        public List<Item> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
